7172b21e501104033850839e8fb64e9615894430
SurendraYalakala <surendrayalakala@gmail.com>
2023-05-17 20:05:58 +0800

    jsx

---
+++ JSX.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// react element
+const heading = <h1 className="parent">Hello React</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
